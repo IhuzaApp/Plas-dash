74b74ab1170b56db72f2713c19cb63951a91f532
Abeltony-prog <abeltony03@gmail.com>
2025-05-23 04:06:43 +0200

    fix on the page aligment

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -19,7 +19,7 @@ const AdminLayout = ({ children }: AdminLayoutProps) => {
     <SidebarProvider>
       <div className="min-h-screen flex w-full bg-background">
         <AdminSidebar isSidebarOpen={isSidebarOpen} />
-        <div className={`flex flex-col flex-1 transition-all ${isSidebarOpen ? 'md:ml-64' : 'md:ml-20'}`}>
+        <div className={`flex flex-col flex-1 transition-all ${isSidebarOpen ? 'md:ml-4' : 'md:-ml-36'}`}>
           <AdminHeader toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
           <main className="flex-1 p-4 md:p-6 overflow-auto">
             {children}
