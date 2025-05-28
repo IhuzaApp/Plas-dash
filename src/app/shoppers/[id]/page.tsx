f360468b1ca6f0b1533eb5f2fcf51587c273976c
Abeltony-prog <abeltony03@gmail.com>
2025-05-28 05:45:09 +0200

    fix: Add type safety check for params in ShopperDetailsPage

--- src/app/shoppers/[id]/page.tsx
+++ src/app/shoppers/[id]/page.tsx
@@ -5,6 +5,9 @@ import { useParams } from 'next/navigation';
 
 export default function ShopperDetailsPage() {
   const params = useParams();
+  if (!params?.id) {
+    return <div>Invalid shopper ID</div>;
+  }
   const shopperId = params.id as string;
 
   return <ShopperDetails shopperId={shopperId} />;
